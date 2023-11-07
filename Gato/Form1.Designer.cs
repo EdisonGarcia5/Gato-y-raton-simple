de99d4db84f75425304cfa7288e44de92340a906
EdisonGarcía5 <edisongarcia542003@gmail.com>
2023-11-07 15:27:30 -0500

    Agregar archivos de proyecto.

---
+++ Gato/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Gato
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.Nuevojuego = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Cerrar = new System.Windows.Forms.Button();
+            this.Reiniciar = new System.Windows.Forms.Button();
+            this.IniciarJuego = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 108);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(144, 172);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 108);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(254, 172);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 108);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(34, 282);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 108);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(144, 282);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(107, 108);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(254, 282);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(107, 108);
+            this.button6.TabIndex = 3;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(34, 392);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(107, 108);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(144, 392);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(107, 108);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(254, 392);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(107, 108);
+            this.button9.TabIndex = 6;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // Nuevojuego
+            // 
+            this.Nuevojuego.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.Nuevojuego.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nuevojuego.Location = new System.Drawing.Point(668, 353);
+            this.Nuevojuego.Name = "Nuevojuego";
+            this.Nuevojuego.Size = new System.Drawing.Size(219, 45);
+            this.Nuevojuego.TabIndex = 9;
+            this.Nuevojuego.Text = "Nuevo Juego";
+            this.Nuevojuego.UseVisualStyleBackColor = false;
+            this.Nuevojuego.Click += new System.EventHandler(this.Nuevojuego_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(649, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 42);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "🐱: 0    🐭: 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(441, 269);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 32);
+            this.label2.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(187, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(627, 54);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "El juego del Gato y el Ratón";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(395, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(227, 42);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Puntuacion ";
+            // 
+            // Cerrar
+            // 
+            this.Cerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Cerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cerrar.Location = new System.Drawing.Point(739, 484);
+            this.Cerrar.Name = "Cerrar";
+            this.Cerrar.Size = new System.Drawing.Size(148, 46);
+            this.Cerrar.TabIndex = 20;
+            this.Cerrar.Text = "Finalizar ";
+            this.Cerrar.UseVisualStyleBackColor = false;
+            this.Cerrar.Click += new System.EventHandler(this.Cerrar_Click_1);
+            // 
+            // Reiniciar
+            // 
+            this.Reiniciar.BackColor = System.Drawing.Color.SpringGreen;
+            this.Reiniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Reiniciar.Location = new System.Drawing.Point(716, 421);
+            this.Reiniciar.Name = "Reiniciar";
+            this.Reiniciar.Size = new System.Drawing.Size(171, 46);
+            this.Reiniciar.TabIndex = 19;
+            this.Reiniciar.Text = "Reiniciar ";
+            this.Reiniciar.UseVisualStyleBackColor = false;
+            this.Reiniciar.Click += new System.EventHandler(this.Reiniciar_Click_1);
+            // 
+            // IniciarJuego
+            // 
+            this.IniciarJuego.BackColor = System.Drawing.Color.Aquamarine;
+            this.IniciarJuego.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IniciarJuego.Location = new System.Drawing.Point(659, 290);
+            this.IniciarJuego.Name = "IniciarJuego";
+            this.IniciarJuego.Size = new System.Drawing.Size(228, 47);
+            this.IniciarJuego.TabIndex = 21;
+            this.IniciarJuego.Text = "Iniciar Juego";
+            this.IniciarJuego.UseVisualStyleBackColor = false;
+            this.IniciarJuego.Click += new System.EventHandler(this.IniciarJuego_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(637, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(276, 32);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Usuario    Maquina ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(962, 551);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.IniciarJuego);
+            this.Controls.Add(this.Cerrar);
+            this.Controls.Add(this.Reiniciar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Nuevojuego);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button Nuevojuego;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Cerrar;
+        private System.Windows.Forms.Button Reiniciar;
+        private System.Windows.Forms.Button IniciarJuego;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Gato/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gato
+{
+    public partial class Form1 : Form
+    {
+        // variables a utilizar en el codigo.
+        Button[] buttons;
+
+        int puntosGato = 0;
+        int puntosRaton = 0;
+
+        bool Turno = true;
+        int movimientos = 0;
+        bool jugando = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Activa los botones con un Arreglo anidado para almacenar los botones 
+            buttons = new Button[]
+            {
+            button1, button2, button3,
+            button4, button5, button6,
+            button7, button8, button9
+            };
+            
+            
+            foreach (Button button in buttons)
+            {
+                button.Enabled = false; // Desactivar los botones del tablero
+                button.Click += BotonClick; // lee la clase y la utiliza en el form1
+            }
+            
+        }
+
+
+        // Sector 1
+        // Clase donde se aplica la logica del juego.
+        private void BotonClick(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender; // se obtiene el botón que generó el evento a través del parámetro "sender" (obtene el objeto que envió el evento).
+
+            if (jugando) // se verifica si se está jugando y ya esta activado como verdadero para iniciar.
+            {
+                if (Turno) // verifica el turno actual
+                {
+                    boton.Text = "X"; // jugador 
+                }
+                else
+                {
+                    boton.Text = "O"; // maquina
+                }
+
+                Turno = false;
+                movimientos++; //lee los espacios disponibles.
+                
+                if (Combinaciones()) // se verifica si se ha alcanzado una combinación ganadora llamando a la función.
+                {
+                    // se utiliza la variable ganador para indicar el mensaje correspondiente y actualizar la etiqueta.
+                    string ganador;
+                    if (!Turno)
+                    {
+                        ganador = "Gato";
+                        puntosGato += 1;
+                    }
+                    else
+                    {
+                        ganador = "Ratón";
+                        puntosRaton += 1;
+                    }
+
+                    MessageBox.Show("El ganador es el Usuario (" + ganador + ")");
+
+                    label1.Text = "🐱: " + puntosGato + "    🐭: " + puntosRaton;
+
+                    jugando = false;
+                }
+                else if (movimientos == 9) // Si no hay una combinación ganadora y se han realizado 9 movimientos, se muestra un mensaje.
+                {
+                    MessageBox.Show("¡Empate!");
+                    jugando = false;
+                }
+
+                // se llama a la función "JugarMaquina()" para que la máquina realice su movimiento.
+                if (jugando && !Turno)
+                {
+                    JugarMaquina();
+                }
+            }
+
+        }
+
+
+        // Sector 2
+        // mejora y bloque las jugadas del usuario 
+        private void JugarMaquina()
+        {
+            // seleccionar aleatoriamente un botón de un arreglo llamado "buttons" y almacenarlo en la variable "boton". 
+            Random random = new Random();
+            int indice = random.Next(buttons.Length);
+            Button boton = buttons[indice];
+
+            while (boton.Text != "") //se verifica si el texto del botón seleccionado es vacío. Si no es vacío, se genera un nuevo número aleatorio y se selecciona otro botón del arreglo. 
+            {
+                indice = random.Next(buttons.Length);
+                boton = buttons[indice];
+            }
+
+            // Original 
+            // Verificar si el usuario está a punto de ganar en su próximo movimiento
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                if (buttons[i].Text == "")
+                {
+                    buttons[i].Text = "X"; // Simular movimiento del usuario
+                    if (Combinaciones())
+                    {
+                        boton = buttons[i]; // Bloquear la jugada del usuario
+                        break;
+                    }
+                    buttons[i].Text = ""; // Deshacer simulación del movimiento del usuario
+                }
+            }
+
+            // Hacer el movimiento de la máquina
+            boton.Text = "O";
+            Turno = true;
+            movimientos++;
+
+            // Verificar si la máquina ganó o hubo empate
+            if (Combinaciones())
+            {
+                MessageBox.Show("El ganador es la Maquina (Ratón)");
+                puntosRaton += 1;
+                label1.Text = "🐱: " + puntosGato + "    🐭: " + puntosRaton;
+                jugando = false;
+            }
+            else if (movimientos == 9)
+            {
+                MessageBox.Show("¡Empate!");
+                jugando = false;
+            }
+        }
+        
+
+        // Sector 3
+        // Verificar todas las combinaciones ganadoras
+        private bool Combinaciones()
+        {
+            if  (
+                (buttons[0].Text == buttons[1].Text && buttons[1].Text == buttons[2].Text && buttons[0].Text != "") ||
+                (buttons[3].Text == buttons[4].Text && buttons[4].Text == buttons[5].Text && buttons[3].Text != "") ||
+                (buttons[6].Text == buttons[7].Text && buttons[7].Text == buttons[8].Text && buttons[6].Text != "") ||
+
+                (buttons[0].Text == buttons[3].Text && buttons[3].Text == buttons[6].Text && buttons[0].Text != "") ||
+                (buttons[1].Text == buttons[4].Text && buttons[4].Text == buttons[7].Text && buttons[1].Text != "") ||
+                (buttons[2].Text == buttons[5].Text && buttons[5].Text == buttons[8].Text && buttons[2].Text != "") ||
+
+                (buttons[0].Text == buttons[4].Text && buttons[4].Text == buttons[8].Text && buttons[0].Text != "") ||
+                (buttons[2].Text == buttons[4].Text && buttons[4].Text == buttons[6].Text && buttons[2].Text != "") 
+                )
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        // Sector 4 botones 
+        private void IniciarJuego_Click(object sender, EventArgs e)
+        {
+            foreach (Button button in buttons)
+            {
+                button.Enabled = true; // Activar los botones del tablero
+            }
+        }
+        
+        private void Nuevojuego_Click(object sender, EventArgs e)
+        {
+            foreach (Button button in buttons)
+            {
+                button.Text = "";
+            }
+
+            movimientos = 0;
+            jugando = true;
+            Turno = true;
+        }
+        
+        private void Reiniciar_Click_1(object sender, EventArgs e)
+        {
+            foreach (Button button in buttons)
+            {
+                button.Enabled = false; // Desactivar los botones del tablero
+                button.Text = "";
+            }
+
+            movimientos = 0;
+
+            jugando = true;
+            Turno = true;
+
+            puntosGato = 0;
+            puntosRaton = 0;
+
+            label1.Text = "🐱: " + puntosGato + "    🐭: " + puntosRaton;
+        }
+
+        private void Cerrar_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
+
+    
